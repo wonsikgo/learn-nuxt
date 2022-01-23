00df5ec147463a45208abde33fd4b85af9d2679d
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-01-23 22:41:50 +0900

    메인페이지 스타일링

--- learn-nuxt/pages/index.vue
+++ learn-nuxt/pages/index.vue
@@ -1,9 +1,70 @@
 <template>
-  <div>
-    <p>넉스트 시작하기</p>
+  <div class="app">
+    <main>
+      <div>
+        <input type="text" />
+      </div>
+      <ul>
+        <li v-for="product in products" :key="product.id" class="item flex">
+          <img
+            :src="product.imageUrl"
+            :alt="product.name"
+            class="product-image"
+          />
+          <p>{{ product.name }}</p>
+          <span0>{{ product.price }}</span0>
+        </li>
+      </ul>
+    </main>
   </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+export default {
+  async asyncData() {
+    const response = await axios.get('http://localhost:3000/products')
+    const products = response.data.map((item) => {
+      return {
+        ...item,
+        imageUrl: `${item.imageUrl}?random=${Math.random()}`,
+      }
+    })
+    return { products }
+  },
+}
 </script>
+
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: center;
+}
+.item {
+  display: inline-block;
+  width: 400px;
+  height: 300px;
+  text-align: center;
+  margin: 0 0.5rem;
+  cursor: pointer;
+}
+.product-image {
+  width: 400px;
+  height: 250px;
+}
+.app {
+  position: relative;
+}
+.cart-wrapper {
+  position: sticky;
+  float: right;
+  bottom: 50px;
+  right: 50px;
+}
+.cart-wrapper .btn {
+  display: inline-block;
+  height: 40px;
+  font-size: 1rem;
+  font-weight: 500;
+}
+</style>
