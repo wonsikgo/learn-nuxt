a632f0659601de0d2f71d4f52395c8bce52fe1b8
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-01-24 00:34:09 +0900

    검색 컴포넌트 추가, 스타일링, 검색기능

--- learn-nuxt/layouts/default.vue
+++ learn-nuxt/layouts/default.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
-    <header>
-      <h1>
-        <NuxtLink to="/">Nuxt Shopping</NuxtLink>
-      </h1>
+    <header class="title">
+      <NuxtLink to="/">Nuxt Shopping</NuxtLink>
     </header>
     <Nuxt />
   </div>
@@ -13,4 +11,18 @@
 export default {}
 </script>
 
-<style></style>
+<style scoped>
+/* header 관련 스타일 */
+header {
+  height: 60px;
+  display: flex;
+  align-items: center;
+  padding: 0 0.5rem;
+}
+.logo {
+  font-weight: 900;
+}
+.logo:visited {
+  color: inherit;
+}
+</style>
--- learn-nuxt/pages/index.vue
+++ learn-nuxt/pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="app">
     <main>
-      <div>
-        <input type="text" />
-      </div>
+      <!-- :search-keyword="searchKeyword"
+        @input="updateSearchKeyword" -->
+      <SearchInput v-model="searchKeyword" @search="searchProducts" />
       <ul>
         <li
           v-for="product in products"
@@ -26,7 +26,11 @@
 
 <script>
 import axios from 'axios'
+import SearchInput from '../components/SearchInput.vue'
+import { fetchProductByKeyword } from '@/api/index'
+
 export default {
+  components: { SearchInput },
   async asyncData() {
     const response = await axios.get('http://localhost:3000/products')
     const products = response.data.map((item) => {
@@ -37,10 +41,27 @@ export default {
     })
     return { products }
   },
+  data() {
+    return {
+      searchKeyword: '',
+    }
+  },
   methods: {
     moveToDetailPage(id) {
       this.$router.push(`detail/${id}`)
     },
+    async searchProducts() {
+      const response = await fetchProductByKeyword(this.searchKeyword)
+      this.products = response.data.map((item) => {
+        return {
+          ...item,
+          imageUrl: `${item.imageUrl}?random=${Math.random()}`,
+        }
+      })
+    },
+    // updateSearchKeyword(keyword) {
+    //   this.searchKeyword = keyword
+    // },
   },
 }
 </script>
